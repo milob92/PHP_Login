053e20911ed535c92f675e22c351cbb432c2b131
milob92 <milob92@gmail.com>
2018-07-25 17:50:00 -0500

    Names
    Put in some names

--- login.php
+++ login.php
@@ -10,16 +10,16 @@
 <body>
   <div class="container">
     <div class="col-xs-6">
-      <form action="">
+      <form action="login.php" method="post">
 
         <div class="form-group">
           <label for="username">Username: </label>
-          <input type="text" class="form-control">
+          <input type="text" name="username"class="form-control">
         </div>
 
         <div class="form-group">
           <label for="password">Password: </label>
-          <input type="password" class="form-control">
+          <input type="password" name="password" class="form-control">
         </div>
 
         </form>
