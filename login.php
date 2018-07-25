871b4c53aa8fa142ff09d07731200e3ca3438047
milob92 <milob92@gmail.com>
2018-07-25 11:28:37 -0500

    Head
    Added sourced bootstrap and added meta

--- login.php
+++ login.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <meta charset="utf-8">
   <title>Login</title>
+  <<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7
+  /css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7
+  on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
 
